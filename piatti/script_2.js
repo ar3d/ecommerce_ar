9c9536c785629e7f5fc43abcc5811352ea77723b
ar3d <info@ar3d.it>
2021-02-11 18:05:05 +0100

    piatti

---
+++ piatti/script_2.js
@@ -0,0 +1,30 @@
+let myusdz = [
+    'piatto/piatto_blu.usdz', 
+    'piatto/piatto_nero.usdz', 
+    'piatto/piatto_rosso.usdz', 
+    'piatto/piatto_verde.usdz'
+];
+
+var model = document.getElementById('model');
+function toggleB(){
+    model.src = 'piatto/piatto_blu.glb';
+    // model.iosSrc === myusdz[0];
+}
+
+var model = document.getElementById('model');
+function toggleN(){
+    model.src = 'piatto/piatto_nero.glb';
+    // model.iosSrc === myusdz[1];
+}
+
+var model = document.getElementById('model');
+function toggleR(){
+    model.src = 'piatto/piatto_rosso.glb';
+    // model.iosSrc === myusdz[2];
+}
+
+var model = document.getElementById('model');
+function toggleV(){
+    model.src = 'piatto/piatto_verde.glb';
+    // model.iosSrc === myusdz[3];
+}
